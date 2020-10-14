466e8349cbef7404858749de218875964253e10a
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-10-14 14:27:07 -0500

    backend completed

--- index.js
+++ index.js
@@ -1,12 +1,26 @@
-const express = require('express')
+const express = require('express');
+const apiRoutes = require("./api-routes");
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const connectDatabase = require('./config/db');
+const port = process.env.PORT || 5000;
 
 //initialize app
 const app = express();
 
-const port = process.env.PORT || 5000;
+//connect database
+connectDatabase();
+
+//config body parser
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.use(bodyParser.json());
+
+//use api routes
+app.use('/api', apiRoutes)
 
 app.get('/', (req, res) =>
-    res.send('hello')
+    res.send('hellooooo')
 );
 
 app.listen(port, () => console.log (`Express server running on port ${port}`));
