eafcd43c918db1c0aec400844908360748a1019e
jaimedue <braxtonkeil@Braxtons-MacBook-Pro.local>
2020-10-14 18:19:14 -0500

    some changes

--- contactController.js
+++ contactController.js
@@ -21,7 +21,6 @@ exports.index = function (req, res) {
 exports.new = function (req, res) {
     var contact = new Contact();
     contact.name = req.body.name ? req.body.name : contact.name;
-    contact.gender = req.body.gender;
     contact.email = req.body.email;
     contact.phone = req.body.phone;
 // save the contact and check for errors
@@ -52,8 +51,7 @@ exports.update = function (req, res) {
 Contact.findById(req.params.contact_id, function (err, contact) {
         if (err)
             res.send(err);
-contact.name = req.body.name ? req.body.name : contact.name;
-        contact.gender = req.body.gender;
+        contact.name = req.body.name ? req.body.name : contact.name;
         contact.email = req.body.email;
         contact.phone = req.body.phone;
 // save and check for errors
